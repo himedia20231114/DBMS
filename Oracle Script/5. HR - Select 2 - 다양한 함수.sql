57fc4ec5fadaf8e9be3fbe3ab572de44aa8f22a5
Jangwoo Park <p.jangwoo@gmail>
2023-12-07 17:35:01 +0900

    12월 07일 - 저녁

--- Oracle Script/5. HR - Select 2 - 다양한 함수.sql
+++ Oracle Script/5. HR - Select 2 - 다양한 함수.sql
@@ -164,6 +164,112 @@ select MOD ( 3, 2 ),            -- 나머지
             TRUNC ( 31 / 2 )        -- 몫만 출력       
 from dual ; 
 
+/*
+ 날짜 함수 : 
+      - sysdate : 현재 시스템의 날짜를 출력 하는 함수 
+      - months_between : 두 날짜 사이의 개월수를 출력 
+      - add_months : 특정 날짜에서 개월수를 더 해서 출력 
+      - next_day : 특정 날짜에서 다음에 초래하는 요일을 인자로 받아서 도래하는 날짜를 출력 
+      - last_day : 달의 마지막 날짜를 출력 
+      - round (날짜) : 날짜를 반올림 : 15일 이상 반올림, 15일 미만 : 삭제 
+      - trunc (날짜) :  날짜를 잘라내는 함수 
+
+*/ 
+
+select sysdate from dual;    -- 23/12/07 
+
+-- 날짜에 연산이 가능  : 
+select sysdate 현재날짜 , sysdate-1 어제날짜, sysdate + 1 내일날짜 from dual ; 
+
+-- 오늘에서 100일전 날짜가 ?
+select sysdate -100 "100전날짜" from dual; 
+-- 오늘 1000일후의 날짜는 ? 
+select sysdate +1000  "1000일후날짜" from dual; 
+
+-- 입사일에서 오늘날까지 총근무일수를 구함.   date  컬럼.     (sysdate - hiredate) 
+desc employee; 
+
+-- 총근일수 = trunc(오늘날짜 - 입사날짜 )
+select ename 이름 , trunc(sysdate - hiredate) 총근무일수  from employee; 
+
+-- 입사일에서 1000일 시점의 날짜  의 이름, 1000일시점의 날짜를 출력 
+select ename 이름 , hiredate 입사날짜,  hiredate + 1000 입사후1000일날짜 from employee; 
+
+-- 특정 날짜에서 월을 기준으로  출력 : TRUNC (날짜, 'MONTH')  , ROUND ( 날짜, 'MONTH') 
+select hiredate 원본날짜 , TRUNC (hiredate, 'MONTH'), ROUND(hiredate, 'MONTH') from employee; 
+
+-- 현재 까지의 근무 개월수를 출력 : months_between (날짜, 날짜)   :  두 날짜 사이의 개월수 출력
+select ename 이름, hiredate 입사날짜, trunc (months_between (sysdate , hiredate)) 개월수 from employee; 
+
+-- 오늘날짜에서 100개월 이후의 날짜 (add_months)  , 100일 후의 날짜 
+-- date 타입 일때 가능
+
+select sysdate 오늘날짜, add_months (sysdate, 100) "100개월후" , sysdate + 100 "100일후" from dual; 
+
+-- last_day : 그 날짜의 마지막 날짜를 출력함 
+select last_day(sysdate) from dual; 
+
+-- 모든 사원의 입사한날의 마지막 날짜가 무엇인지 출력 
+select hiredate 입사날짜, last_day(hiredate) 마지막날짜 from employee; 
+
+
+/*
+    형식 변환 함수  
+        TO_CHAR : 날짜형, 숫자형 ===> 문자형 
+        TO_DATE : 문자 ====> 날짜형식으로 변환 
+        TO_NUMBER : 문자 ==> 숫자형식으로 변환 
+*/
+
+-- TO_CHAR  : 날짜 ==> 문자 
+
+select sysdate from dual;    --  23/12/07 
+
+-- YYYY : 년도 , MM :월 , MON: 월  , DD : 일 , HH : 시간 , MI : 분 , SS : 초 , DAY : 요일 (월요일), DY(월, 화) 
+select TO_CHAR (sysdate , 'YYYY-MM-DD HH:MI:SS') from dual ; 
+
+select hiredate 입사날짜  , TO_CHAR (hiredate , 'YYYY"년" MI"월" DD"일" HH:mm:SS') 입사날짜2
+from employee; 
+
+select TO_CHAR (sysdate , 'YYYY-MM-DD MON DAY DY HH:MI:SS') from dual ; 
+
+-- TO_CHAR  ; 숫자  ==> 문자 
+/*
+        0 : 자릿수를 처리함, 자릿수가 많으면 0으로 처리됨 
+        9 : 자릿수를 처리함, 자릿수가 많으면 공백으로 처리됨 
+        L: 각 지역의 통화를 기호로 표시함. 
+        
+        . :  소숫점으로 처리됨 
+        , :  천단위 구분자 
+*/ 
+
+select TO_CHAR ( 9876 , 'L000,000') from dual ; 
+select TO_CHAR ( 9876 , 'L999,999') from dual ; 
+
+-- 월급을 그 나라의 통화 기록을 붙여서 천단위로 출력 
+
+select salary 월급 , to_char(salary , 'L999,999') "월급(한국)"
+from employee; 
+
+
+-- TO_DATE : 문자 , 숫자 ==> 날짜형식으로 바꿈. 
+     -- 날짜 + 100일 
+     -- months_between(날짜, 날짜) : 개월 수 
+
+select to_date ('1998-10-10' , 'YYYY-MM-DD') from dual ; 
+
+-- 1981년 01월 01일 에서 100일 지난 시점의 날짜를 출력 , 100개월 지난 시점의 날짜 , add_month(날짜, 개월수) 
+
+select to_date ('1981/01/01', 'YYYY/MM/DD') + 100 "100일후 날짜" , 
+           add_months( to_date ('1981/01/01', 'YYYY/MM/DD') , 100)  "100개월이후의 날짜"
+from dual; 
+
+-- 숫자 ==> 날짜형식으로 변환 
+select to_date ( 770814 , 'YYMMDD') from dual 
+
+-- 자신의 생일에서 현재까지 몇개월 살았는지 ?  몇일 살았는지 출력   <== months_between ( 현재날짜, 생일)  : 개월수 
+        -- sysdate - 생일 (date) 
+        
+-- employee 테이블에서 2050년 12월 24 일까지의 날짜(일수)  를 출력 
 
 
 
