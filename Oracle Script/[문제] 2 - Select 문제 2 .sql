ca7e47044b9f6ae0b245014259f22d91aa6ec28f
Jangwoo Park <p.jangwoo@gmail>
2023-12-08 10:25:35 +0900

    12월 08일 아침2

--- Oracle Script/[문제] 2 - Select 문제 2 .sql
+++ Oracle Script/[문제] 2 - Select 문제 2 .sql
@@ -39,10 +39,16 @@ from employee;
 2. 사원의 이름, 급여, 연간 총 수입이 많은것 부터 작은순으로 출력 하시오. 
     연간 총 수입은 월급에 12를 곱한후 $100의 상여금을 더해서 계산 하시오. 
     
+    -- 연결된 DataBase 에서 모든 테이블 출력 
+    select * from tab; 
+    
     select ename, salary, commission from employee; 
     
     select ename, salary,commission,salary *12, salary *12 + NVL(commission,0) + 100 as 총연봉
-    from employee;   
+    from employee
+    order by salary *12 + NVL(commission,0) + 100 desc; 
+    
+    
      
 3. 급여가 2000을 넘는 사원의 이름과 급여를 급여가 많은것 부터 작은순으로 출력하세요. 
 
@@ -52,6 +58,8 @@ select ename, salary    -- 컬럼명
 from employee           -- 테이블, 뷰
 where salary >= 2000    -- 조건
 order by salary desc    -- 정렬 
+
+
 
 4. 사원번호가 7788인 사원의 이름과 부서번호를 출력하세요. 
 select ename, dno ,eno
@@ -73,12 +81,12 @@ where hiredate between '1981/2/20' and '1981/5/1';
 select ename, dno 
 from employee
 where dno = 20 or dno = 30 
-order by ename asc; 
+order by ename desc; 
 
 select ename, dno 
 from employee
 where dno in ( 20, 30 )
-order by ename asc; 
+order by ename desc; 
 
 8. 사원의 급여가 2000에서 3000사이에 포함되고 부서번호가 20 또는 30인 사원의 이름, 급여와 부서번호를 출력하되 이름을 오름차순으로 출력하세요. 
 select ename, salary, dno 
@@ -89,6 +97,10 @@ where (salary between 2000 and 3000) and (dno in (20,30))
 select ename, hiredate 
 from employee 
 where hiredate like '81%';
+
+select ename, hiredate 
+from employee 
+where substr (hiredate , 1,2) = '81' ;
 
 
 10. 관리자가 없는 사원의 이름과 담당업무를 출력하세요.
@@ -111,7 +123,7 @@ order by salary desc, commission desc
 12. 이름이 세번째 문자인 R인 사원의 이름을 표시하시오.
 select ename 
 from employee
-where ename like '__R%' 
+where ename like '__R%'      
 
 select ename
 from employee
@@ -129,12 +141,14 @@ select ename
 from employee
 where instr(ename, 'A')!=0 and instr(ename, 'E')!=0;
 
+select ename , instr (ename, 'A') , instr(ename , 'E') from employee; 
+
 14. 담당 업무가 사무원(CLERK) 또는 영업사원(SALESMAN)이며서 
     급여가 $1600, $950, 또는 $1300 이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
 
 select ename, job, salary 
 from employee
-where (job = 'CLERK' or job = 'SALESMAN') and salary not in ( 1600,950, 1300) 
+where (job = 'CLERK' or job = 'SALESMAN') and  salary not in ( 1600, 950, 1300) 
 
 select ename , job , salary 
 from employee
